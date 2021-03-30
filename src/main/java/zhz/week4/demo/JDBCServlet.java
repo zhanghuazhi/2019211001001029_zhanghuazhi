db241bce3fd7bb6bbab2f2c17a67f106540769c1
zhanghuazhi <1766963292@qq>
2021-03-30 21:27:31 +0800

    HW-Week 4 commit

---
+++ src/main/java/zhz/week4/demo/JDBCServlet.java
@@ -0,0 +1,74 @@
+package zhz.week4.demo;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(
+        urlPatterns = {"/jdbc"},
+        initParams = {
+                @WebInitParam(name="driver",value="com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+                @WebInitParam(name="url",value="jdbc:sqlserver://localhost:1433;databaseName=userdb;"),
+                @WebInitParam(name="username",value="sa"),
+                @WebInitParam(name="password",value="123456789"),
+        },loadOnStartup = 1
+)
+public class JDBCServlet extends HttpServlet {
+    Connection con=null;
+    @Override
+    public void init() throws ServletException {
+        // String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        // String url="jdbc:sqlserver://localhost;databaseName=userdb;";
+        // String username="sa";
+        // String password="123456";
+
+        ServletConfig config=getServletConfig();
+        String driver=config.getInitParameter("driver");
+        String url=config.getInitParameter("url");
+        String username=config.getInitParameter("username");
+        String password=config.getInitParameter("password");
+        try{
+            Class.forName(driver);
+            con= DriverManager.getConnection(url,username,password);
+            System.out.println("init()--> "+con);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("i am in doGet()");
+     /*   String sql="select * from usertable";
+        try {
+            ResultSet rs=con.createStatement().executeQuery(sql);
+            while (rs.next()){
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }  */
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
